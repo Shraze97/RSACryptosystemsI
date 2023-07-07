1bf62a587d38ff6b1bb6057dcdff6ae4b54028be
Suryansh Shrivastava <suryanshs@iisc.ac.in>
2023-07-07 23:24:34 +0530

    execution process

--- Main.lean
+++ Main.lean
@@ -1,8 +1,9 @@
-import «RSACryptosystemsI»
-import RSACryptosystemsI.MainStructure
+-- import «RSACryptosystemsI»
+-- import RSACryptosystemsI.MainStructure
 
 def main (args : List String) : IO Unit := do 
   let stdin ← IO.getStdin
   let stdout ← IO.getStdout
   IO.print "Enter the message to be encrypted "
 
+#eval main []
---
+++ RSACryptosystemsI/IO.lean
@@ -0,0 +1,7 @@
+
+def main (args : List String) : IO Unit := do 
+  let stdin ← IO.getStdin
+  IO.println "Enter the message to be encrypted "
+  let input ← stdin.getLine
+  
+  IO.println s!"The message is {input}"
